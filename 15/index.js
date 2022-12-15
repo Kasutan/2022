95bef6936f586c506ddc89613b085b05582eae50
Kasutan <contact@kasutan.pro>
2022-12-15 16:16:28 +0100

    Jour 15 - 2e partie

--- 15/index.js
+++ 15/index.js
@@ -1,7 +1,6 @@
 let sources = [];
 let beacons = [];
-let cible = 2000000;
-let beaconsSurCible= [];
+//let beaconsSurCible= [];
 let intervalles = [];
 
 
@@ -11,7 +10,7 @@ function readFile(input) {
 	reader.readAsText(f);
 	reader.onload = function() {
 		let texte=reader.result.replace(/(?:\r|\n|\r\n)/g, '!');
-		afficheTotal(texte)
+		afficheTotal(texte,2000000)
 	};
 	reader.onerror = function() {
 		console.log(reader.error);
@@ -21,45 +20,48 @@ function readFile(input) {
 /*Exemple*/
 let inputExemple="Sensor at x=2, y=18: closest beacon is at x=-2, y=15!Sensor at x=9, y=16: closest beacon is at x=10, y=16!Sensor at x=13, y=2: closest beacon is at x=15, y=3!Sensor at x=12, y=14: closest beacon is at x=10, y=16!Sensor at x=10, y=20: closest beacon is at x=10, y=16!Sensor at x=14, y=17: closest beacon is at x=10, y=16!Sensor at x=8, y=7: closest beacon is at x=2, y=10!Sensor at x=2, y=0: closest beacon is at x=2, y=10!Sensor at x=0, y=11: closest beacon is at x=2, y=10!Sensor at x=20, y=14: closest beacon is at x=25, y=17!Sensor at x=17, y=20: closest beacon is at x=21, y=22!Sensor at x=16, y=7: closest beacon is at x=15, y=3!Sensor at x=14, y=3: closest beacon is at x=15, y=3!Sensor at x=20, y=1: closest beacon is at x=15, y=3";
 
-afficheTotal(inputExemple);
+//afficheTotal(inputExemple,10);
 
-function afficheTotal(texte) {
+function afficheTotal(texte,milieu) {
 	sources = [];
 	beacons = [];
-	cible = 2000000;
-	//cible = 10;
-	beaconsSurCible= [];
-	intervalles = [];
+	//beaconsSurCible= [];
 
 
 	parseInput(texte);
-	let beaconsSurCibleU=[... new Set(beaconsSurCible)];
-	console.log('sources',sources);
-	console.log('beacons',beacons);
-	console.log('beacons sur cible',beaconsSurCibleU);
-	
-	detecteIntervalles();
-	intervalles.sort((a,b) => (a.min > b.min));
-	let continuerRegr=true;
-	while(continuerRegr) {
-		continuerRegr=regroupeIntervalles();
+	//let beaconsSurCibleU=[... new Set(beaconsSurCible)];
+	//console.log('sources',sources);
+	//console.log('beacons',beacons);
+	//console.log('beacons sur cible',beaconsSurCibleU);
+	//let n=0;
+	let n=639657;
+	let yBeacon=false;
+	for(n; n < 639660; n++) {
+		console.log('step',n);
+		console.log('cible',milieu+n);
+		
+		intervalles = [];
+		detecteIntervalles(milieu+n);
+		if(intervalles.length > 1) {
+			yBeacon=milieu+n;
+			break;
+		};
+		/*
+
+		intervalles = [];
+		detecteIntervalles(milieu-n);
+		if(intervalles.length > 1) {
+			yBeacon=milieu-n;
+			break;
+		};*/
 	}
-
-	let total=0;
-	intervalles.forEach(element => {
-		total+=element.max - element.min + 1;
-	});
-
-	total=total - beaconsSurCibleU.length;
 	console.log('intervalles',intervalles);
-	document.getElementById('reponse').value=total;
+	let xBeacon=intervalles[0].max + 1;
+	console.log('xBeacon',xBeacon);
+	let frequency=xBeacon * 4000000 + yBeacon
+	document.getElementById('reponse').value=frequency;
 
-	//2e partie de l'exercice
-	/*
-	let total2=tailleSuppr();
-	console.log('total2',total2)
-	document.getElementById('reponse-2').value=total2;
-*/
+	
 }
 
 function parseInput(texte) {
@@ -69,25 +71,26 @@ function parseInput(texte) {
 		ligneS=ligneS.replace(" y=","");
 		ligneS=ligneS.replace(" y=","");
 		ligneS=ligneS.replace(": closest beacon is at x=","$");
-		console.log(ligneS);
+		//console.log(ligneS);
 		let ligne=ligneS.split('$');
 		let source=ligne[0];
 		let beacon=ligne[1];
 		sources.push(source);
 		beacons.push(beacon);
+		/*
 		let beaconY=parseInt(beacon.split(',')[1]);
 		if(beaconY===cible) {
 			beaconsSurCible.push(beacon);
-		}
+		}*/
 		
 	})
 }
 
 
 
-function detecteIntervalles() {
+function detecteIntervalles(cible) {
 	sources.forEach((s,index) => {
-		console.log('==========source',s);
+		//console.log('==========source',s);
 		let b=beacons[index];
 		//console.log('beacon',b);
 		let sx=parseInt(s.split(',')[0]);
@@ -98,10 +101,7 @@ function detecteIntervalles() {
 		//console.log('r',r);
 
 		let deltaY=Math.abs(sy - cible);
-		if(deltaY > r) {
-			console.log("cette source n'atteint pas la cible")
-		} else {
-			
+		if(deltaY <= r) {
 			let xMax = sx + r - deltaY;
 			let xMin = sx - r + deltaY;
 			//console.log('xMin',xMin);
@@ -112,6 +112,12 @@ function detecteIntervalles() {
 		}
 		
 	})
+
+	intervalles.sort((a,b) => (a.min > b.min));
+	let continuerRegr=true;
+	while(continuerRegr) {
+		continuerRegr=regroupeIntervalles();
+	}
 }
 
 function calculateR(sx,sy,bx,by) {
